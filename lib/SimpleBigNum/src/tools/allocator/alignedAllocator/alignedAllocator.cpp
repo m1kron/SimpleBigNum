a095c6d48d351c08cac0cbd6f6bff760ffa49de2
m1kron <piotr.kowalczyk102+github@gmail.com>
2020-07-25 11:59:42 +0200

    [sbn]: AlignedAllocator code format.

--- lib/SimpleBigNum/src/tools/allocator/alignedAllocator/alignedAllocator.cpp
+++ lib/SimpleBigNum/src/tools/allocator/alignedAllocator/alignedAllocator.cpp
@@ -12,7 +12,7 @@ static const size_t SIMD_ALIGNMENT = 16;
 /////////////////////////////////////////////////////////////////////////////////////////
 void* AlignedAllocator::Allocate( size_t requestedSize )
 {
-    uintptr_t neededOffset = SIMD_ALIGNMENT + sizeof( void * );
+    uintptr_t neededOffset = SIMD_ALIGNMENT + sizeof( void* );
     void* raw = malloc( requestedSize + neededOffset );
     void** ptr = ( void** )tools::align<uintptr_t, SIMD_ALIGNMENT>( ( uintptr_t )raw + sizeof( void* ) );
     ptr[ -1 ] = raw;
@@ -20,7 +20,7 @@ void* AlignedAllocator::Allocate( size_t requestedSize )
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
-void AlignedAllocator::Free( void * ptr )
+void AlignedAllocator::Free( void* ptr )
 {
     if( ptr != nullptr )
     {
